4d03a9654fa423778ae60dd4e357a5bee8758783
Thomas <git@aeshus.com>
2025-02-10 11:16:19 -0600

    Meh code

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,7 +1,11 @@
 import { Scene } from 'phaser';
 
+let player;
+let speed = 0;
+
 export class Game extends Scene
 {
+
     constructor ()
     {
         super('Game');
@@ -9,20 +13,46 @@ export class Game extends Scene
 
     create ()
     {
-        this.cameras.main.setBackgroundColor(0x00ff00);
+        player = this.add.rectangle(400, 300, 50, 100, 0x00ff00);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
-
-        this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
+	this.physics.add.existing(player)
+	player.body.setCollideWorldBounds(true);
+        player.body.setBounce(20, 20);
+        player.body.setSize(50, 100);
+        player.body.setAllowRotation(true);
+	player.body.setMaxSpeed(10);
 
         this.input.once('pointerdown', () => {
-
             this.scene.start('GameOver');
-
         });
+    }
+
+    update ()
+    {
+       let cursors = this.input.keyboard.createCursorKeys();
+
+       if (cursors.left.isDown) {
+           player.body.angularVelocity -= 3; 
+       } 
+       if (cursors.right.isDown) {
+           player.body.angularVelocity += 3; 
+       }
+
+       if (cursors.up.isDown) {
+          this.physics.velocityFromRotation(player.rotation - Math.PI / 2, speed += 10, player.body.velocity);
+       } else if (cursors.down.isDown) {
+          this.physics.velocityFromRotation(player.rotation - Math.PI / 2, speed += 10, player.body.velocity);
+       } else {
+           if (speed > 0) {
+               speed = Math.max(0, speed - 0.5);
+           } else if (speed < 0) {
+               speed = Math.min(0, speed + 0.5);
+           }
+       }
+
+       // Move the car based on its rotation
+       this.physics.velocityFromRotation(player.rotation - Math.PI / 2, speed, player.body.velocity);
+
+	console.log(speed);
     }
 }
